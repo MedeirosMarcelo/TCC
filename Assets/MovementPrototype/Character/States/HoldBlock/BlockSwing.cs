bb3d9ade219b7a07cbd6776c376baf0465aab2c2
Diogo Ribeiro <diogoriba@gmail.com>
2016-04-28 15:38:38 -0300

    heavy attack beats block

--- Assets/MovementPrototype/Character/States/HoldBlock/BlockSwing.cs
+++ Assets/MovementPrototype/Character/States/HoldBlock/BlockSwing.cs
@@ -57,7 +57,7 @@ namespace Assets.MovementPrototype.Character.States.HoldBlockStates
                 if (!ReferenceEquals(Character, otherCharacter))
                 {
                     var attackerState = otherCharacter.fsm.Current as BaseSwing;
-                    if (attackerState != null)
+                    if (attackerState != null && !(attackerState is HeavySwing))
                     {
                         Vector3 myForward = Transform.forward;
                         Vector3 otherForward = attackerState.Character.transform.forward;
