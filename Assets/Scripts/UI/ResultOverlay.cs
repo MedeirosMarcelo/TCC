db33646d2531e4768b4a6dd1acdb1221b3fd5698
MedeirosMarcelo <marcelomedeiros-@hotmail.com>
2016-06-08 00:38:34 -0300

    Result Screen

---
+++ Assets/Scripts/UI/ResultOverlay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResultOverlay : Menu
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        ActivateOption();
+        base.Update();
+    }
+
+    void ActivateOption()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (cursorIndex)
+            {
+                case 0:
+                    SceneManager.LoadScene("Arena");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Main Menu");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
