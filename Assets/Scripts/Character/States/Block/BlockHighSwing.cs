1fe5ae3a80292bafd054aff6c2b60978372343a7
Diogo Ribeiro <diogoriba@gmail.com>
2016-06-09 19:38:36 -0300

    more defense fixes

--- Assets/Scripts/Character/States/Block/BlockHighSwing.cs
+++ Assets/Scripts/Character/States/Block/BlockHighSwing.cs
@@ -19,18 +19,20 @@ namespace Assets.Scripts.Character.States.Block
                 if (attack != null && attack.Direction == AttackDirection.Vertical)
                 {
                     RaycastHit hitInfo;
-                    Assert.IsTrue(attack.GetCollisionPoint(out hitInfo), "IT SHOULD HAVE HIT BUT IT DID NOT HIT SEND HELP");
-                    Vector3 myForward = Transform.forward.xz().normalized;
-                    Vector3 otherForward = (hitInfo.point - Transform.position).xz().normalized;
-                    if (Mathf.Abs(Vector3.Angle(myForward, otherForward)) <= defenseAngle / 2f)
+                    if (attack.GetCollisionPoint(out hitInfo))
                     {
-                        Character.ShowBlockSpark(collider.transform.position);
-                        attack.Blocked();
-                        if (attack.IsHeavy)
+                        Vector3 myForward = Transform.forward.xz().normalized;
+                        Vector3 otherForward = (hitInfo.point - Transform.position).xz().normalized;
+                        if (Mathf.Abs(Vector3.Angle(myForward, otherForward)) <= defenseAngle / 2f && attack.CanHit(Character))
                         {
-                            Fsm.ChangeState("STAGGER");
+                            Character.ShowBlockSpark(collider.transform.position);
+                            attack.Blocked();
+                            if (attack.IsHeavy)
+                            {
+                                Fsm.ChangeState("STAGGER");
+                            }
+                            return;
                         }
-                        return;
                     }
                 }
             }
